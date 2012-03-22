bd256ae7659cc45717ad7139f183aece033029ea
Stuart Harrison <pezholio@gmail.com>
2012-03-22 14:53:39 +0000

    Contact form

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -8,6 +8,6 @@ class Contact
 	attribute :address
 	attribute :council
 	
-	validates_presence_of :name
+	validates_presence_of :name, :message
 	validates_format_of :email, :with => /^[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}$/i
 end
