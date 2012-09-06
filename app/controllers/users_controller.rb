86e9c3d830084629586d9d23444f531a1d709514
Stuart Harrison <pezholio@gmail.com>
2012-09-06 15:08:43 +0100

    Only allow superadmins to create admins

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   
   def new 
     if session[:user_id]
-    	if current_user.role == 2
+    	if current_user.role >= 2
 	    	@user = User.new
 	    else
 	    	redirect_to '/admin', :notice => "I'm afraid I can't let you do that, Dave" 
@@ -14,8 +14,13 @@ class UsersController < ApplicationController
   end
 
   def create
-  	if current_user.role == 2
-	    @user = User.new(params[:user])
+  	if current_user.role >= 2
+  		@user = User.new
+  		@user.accessible = [:role] if current_user.role >= 2
+  		@user.attributes = params[:user]
+  		if current_user.role == 2
+    		@user.role = 1
+    	end
 	    if @user.save
 	      redirect_to '/admin', :notice => "User created!"
 	    else
@@ -30,7 +35,7 @@ class UsersController < ApplicationController
 
   def update
     @user = current_user
-    @user.accessible = [:role] if @user.role == 2
+    @user.accessible = [:role] if @user.role == 3
     if @user.update_attributes(params[:user])
       redirect_to '/admin', :notice => "Your profile has been updated."
     else
