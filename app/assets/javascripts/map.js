06ab6fb7b61907818030f61882514421576ef964
pezholio <pezholio@gmail.com>
2014-06-30 21:18:24 +0100

    Use OSM map tiles

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
 	var map = new L.Map('map');
-	var cloudmadeUrl = 'http://{s}.tile.cloudmade.com/3a36067fc4f2404eb235c892bb344b06/997/256/{z}/{x}/{y}.png',
-			cloudmadeAttrib = 'Map data &copy; 2011 OpenStreetMap contributors, Imagery &copy; 2011 CloudMade',
-			cloudmade = new L.TileLayer(cloudmadeUrl, {maxZoom: 18, attribution: cloudmadeAttrib});
-		
-		var latlng = new L.LatLng(lat, lng); 
-	map.setView(latlng, 16).addLayer(cloudmade);
-	 
+	var osmUrl = 'http://otile4.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png',
+			osmAttribution = 'Tiles Courtesy of <a href="http://www.mapquest.com/">MapQuest</a> &mdash; Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+			osm = new L.TileLayer(osmUrl, {maxZoom: 18, attribution: osmAttribution});
+
+		var latlng = new L.LatLng(lat, lng);
+	map.setView(latlng, 16).addLayer(osm);
+
 	var marker = new L.Marker(latlng, {icon: new L.Icon('/assets/marker.png')});
 	map.addLayer(marker);
 })
