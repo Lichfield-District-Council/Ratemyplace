fb53153f4352fe1980dc1710b386d2eda2975c6b
pezholio <pezholio@gmail.com>
2014-07-08 19:46:51 +0100

    Fix search map

--- app/assets/javascripts/searchmap.js
+++ app/assets/javascripts/searchmap.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
 	var map = new L.Map('searchmap');
-	var cloudmadeUrl = 'http://{s}.tile.cloudmade.com/3a36067fc4f2404eb235c892bb344b06/997/256/{z}/{x}/{y}.png',
-			cloudmadeAttrib = 'Map data &copy; 2011 OpenStreetMap contributors, Imagery &copy; 2011 CloudMade',
-			cloudmade = new L.TileLayer(cloudmadeUrl, {maxZoom: 18, attribution: cloudmadeAttrib});
-	
-	var latlng = new L.LatLng(52.6839, -1.82598); 
-	map.setView(latlng, 4).addLayer(cloudmade);
+	var osmUrl = 'http://otile4.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png',
+			osmAttribution = 'Tiles Courtesy of <a href="http://www.mapquest.com/">MapQuest</a> &mdash; Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+			osm = new L.TileLayer(osmUrl, {maxZoom: 18, attribution: osmAttribution});
+
+	var latlng = new L.LatLng(52.6839, -1.82598);
+	map.setView(latlng, 4).addLayer(osm);
 	var mapBounds = new L.LatLngBounds();
-	
+
 	for (i in points) {
 		var latlng = new L.LatLng(points[i]['lat'], points[i]['lng']);
 		var marker = new L.Marker(latlng, {icon: new L.Icon('/assets/'+ i +'.png')});
@@ -15,6 +15,6 @@ $(document).ready(function() {
 		map.addLayer(marker);
 		mapBounds.extend(latlng);
 	}
-	
+
 	map.fitBounds(mapBounds);
 })
