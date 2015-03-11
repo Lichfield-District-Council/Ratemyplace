06659e49dac9d54d52690b31d607482867cd7ce0
pezholio <pezholio@gmail.com>
2015-03-11 09:36:42 +0000

    Allow posting to multiple Buffer profiles

--- lib/buffer_app.rb
+++ lib/buffer_app.rb
@@ -1,30 +1,29 @@
 class BufferApp
 	include HTTParty
 	base_uri 'https://api.bufferapp.com/1'
-		
+
 	def initialize(token, id)
 		@token = token
-		@id = id
+		@id = [id].flatten
 	end
-	
+
 	def profiles
 		BufferApp.get('/profiles.json', :query => {"access_token" => @token}).parsed_response
 	end
-	
+
 	def pending
 		BufferApp.get("/profiles/#{@id}/updates/pending.json", :query => {"access_token" => @token}).parsed_response
 	end
-	
+
 	def sent
 		BufferApp.get("/profiles/#{@id}/updates/sent.json", :query => {"access_token" => @token}).parsed_response
 	end
 
 	def create(text, now = false)
 		if now === false
-			BufferApp.post('/updates/create.json', :body => {"text" => text, "profile_ids[]" => @id, "access_token" => @token}).parsed_response
+			BufferApp.post('/updates/create.json', :body => {"text" => text, "profile_ids" => @id, "access_token" => @token}).parsed_response
 		else
-			BufferApp.post('/updates/create.json', :body => {"text" => text, "profile_ids[]" => @id, "access_token" => @token, "now" => "true"}).parsed_response
+			BufferApp.post('/updates/create.json', :body => {"text" => text, "profile_ids" => @id, "access_token" => @token, "now" => "true"}).parsed_response
 		end
-	end	
+	end
 end
-
