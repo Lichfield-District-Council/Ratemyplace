542a10a165b49975e85e085196b651ad8c7dfa58
pezholio <pezholio@gmail.com>
2015-10-29 12:48:00 +0000

    Use new GIS lookup

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,98 +1,126 @@
 class Address < ActiveRecord::Base
-	require 'httparty'
-	require 'json'
-	require 'easting_northing'
-	require 'pat'
-	
-	def latlng
-		latlng = EastingNorthing.eastingNorthingToLatLong(self.x, self.y)
-		results = Hash.new
-		results[:lat] = latlng["lat"]
-		results[:lng] = latlng["long"]
-		return results
-	end
-	
-	def self.GetAddressFromPostcode(postcode)
-	
-		postcode = CGI::escape(postcode)
-	
-		addresses = JSON.parse HTTParty.get("https://www.lichfielddc.gov.uk/site/custom_scripts/countyllpg.php?q=#{postcode}").response.body
-		
-		num = 0
-		results = []
-				
-		addresses.each do |item|
-			results[num] = Hash.new
-			results[num][:uprn] = item["uprn"].to_i
-			results[num][:address] = item["fulladdress"]
-			results[num][:address1] = item["line1"]
-			results[num][:address2] = item["line2"]
-			results[num][:address3] = item["line3"]
-			results[num][:address4] = item["line4"]
-			results[num][:town] = item["town"]
-			results[num][:postcode] = item["postcode"]
-			results[num][:easting] = item["easting"].to_i
-			results[num][:northing] = item["northing"].to_i
-			
-			easting = item["easting"].to_i
-			northing = item["northing"].to_i
-						
-			latlng = EastingNorthing.eastingNorthingToLatLong(easting, northing)
-						
-			results[num][:lat] = latlng["lat"]
-			results[num][:lng] = latlng["long"]
-			
-			num += 1
-		end
-		
-		return results
-	end
-	
-	def self.GetAddressFromUprn(uprn)
-			
-		address = JSON.parse HTTParty.get("https://www.lichfielddc.gov.uk/site/custom_scripts/countyllpg.php?type=uprn&q=#{uprn}").response.body
-		
+  require 'httparty'
+  require 'json'
+  require 'easting_northing'
+  require 'pat'
+
+  def latlng
+    latlng = EastingNorthing.eastingNorthingToLatLong(self.x, self.y)
+    results = Hash.new
+    results[:lat] = latlng["lat"]
+    results[:lng] = latlng["long"]
+    return results
+  end
+
+  def self.address_endpoint(query, layer = "POSTCODE")
+    "http://gis.lichfielddc.gov.uk/arcgis/rest/services/PUBLIC/County_NLPG/MapServer/find?searchText=#{query}&contains=true&layers=#{layer}&returnGeometry=true&f=pjson"
+  end
+
+  def self.coerce_address(address)
+    address.each { |k,v| address[k] = nil if v == "Null" }
+    address
+  end
+
+  def self.first_line(address)
+    sao = addressable_object(address, "SAO")
+    pao = addressable_object(address, "PAO")
+    [
+      address["SAO_TEXT"], sao, address["PAO_TEXT"], pao, address["STREET_DESCRIPTOR"]
+    ].reject!{ |p| p.blank? }.join(" ")
+  end
+
+  def self.full_address(address)
+    [
+      address["FIRST_LINE"], address["LOCALITY_NAME"], address["POST_TOWN"], address["POSTCODE"]
+    ].reject { |p| p.blank?}.join(", ")
+  end
+
+  def self.addressable_object(address, type)
+    s = address["#{type}_START_NUMBER"].to_s + address["#{type}_START_SUFFIX"].to_s
+    e = address["#{type}_END_NUMBER"].to_s + address["#{type}_END_SUFFIX"].to_s
+    if s.blank? && e.blank?
+      nil
+    elsif e.blank?
+      s
+    else
+      "#{s} - #{e}"
+    end
+  end
+
+	def self.build_address(item)
+		address = coerce_address(item["attributes"])
+		address["FIRST_LINE"] = first_line(address)
+
 		result = Hash.new
-		
-		if address.length == 0
-			result = nil
-		else		
-			result[:uprn] = address[0]["uprn"].to_i
-			result[:address] = address[0]["fulladdress"]
-			result[:address1] = address[0]["line1"]
-			result[:address2] = address[0]["line2"]
-			result[:address3] = address[0]["line3"]
-			result[:address4] = address[0]["line4"]
-			result[:town] = address[0]["town"]
-			result[:postcode] = address[0]["postcode"]
-			result[:easting] = address[0]["easting"].to_i
-			result[:northing] = address[0]["northing"].to_i
-			
-			easting = address[0]["easting"].to_i
-			northing = address[0]["northing"].to_i
-			
-			latlng = EastingNorthing.eastingNorthingToLatLong(easting, northing)
-							
-			result[:lat] = latlng["lat"]
-			result[:lng] = latlng["long"]
-		end
-		
-		return result
+		result[:uprn] = address["UPRN"].to_i
+		result[:address] = full_address(address)
+		result[:address1] = address["FIRST_LINE"]
+		result[:address2] = address["LOCALITY_NAME"]
+		result[:address3] = nil
+		result[:address4] = nil
+		result[:town] = address["POST_TOWN"]
+		result[:postcode] = address["POSTCODE"]
+		result[:easting] = item["geometry"]["x"].to_i
+		result[:northing] = item["geometry"]["y"].to_i
+
+		easting = item["geometry"]["x"].to_i
+		northing = item["geometry"]["y"].to_i
+
+		latlng = EastingNorthing.eastingNorthingToLatLong(easting, northing)
+
+		result[:lat] = latlng["lat"]
+		result[:lng] = latlng["long"]
+
+		result
 	end
-	
-	def self.GetLatLngFromPostcode(postcode)
-		
-		result = Hash.new
-		
-		begin	
-			postcode = Pat.get(postcode)
-			result[:lat] = postcode["geo"]["lat"]
-			result[:lng] = postcode["geo"]["lng"]
-		rescue
-			result[:lat] = 0
-			result[:lng] = 0	
-		end
-		
-		return result
-	end
+
+  def self.GetAddressFromPostcode(postcode)
+    postcode = CGI::escape(postcode)
+    url = address_endpoint(postcode)
+
+    addresses = JSON.parse HTTParty.get(url).response.body
+
+    num = 0
+    results = []
+
+    addresses["results"].each do |item|
+			results[num] = build_address(item)
+      num += 1
+    end
+
+    return results.uniq! { |a| a[:address] }.sort_by { |a| a[:address] }
+  end
+
+  def self.GetAddressFromUprn(uprn)
+
+		url = address_endpoint(uprn, "UPRN")
+
+    address = JSON.parse HTTParty.get(url).response.body
+
+    result = Hash.new
+
+    if address.length == 0
+      result = nil
+    else
+      result = build_address(address["results"].first)
+    end
+
+    return result
+  end
+
+  def self.GetLatLngFromPostcode(postcode)
+
+    result = Hash.new
+
+    begin
+      postcode = Pat.get(postcode)
+      result[:lat] = postcode["geo"]["lat"]
+      result[:lng] = postcode["geo"]["lng"]
+    rescue
+      result[:lat] = 0
+      result[:lng] = 0
+    end
+
+    return result
+  end
 end
