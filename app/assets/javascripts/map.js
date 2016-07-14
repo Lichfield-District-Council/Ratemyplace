e3485e81918f7075c9b56cadc965eab98e7e27cb
pezholio <pezholio@gmail.com>
2016-07-14 21:41:55 +0100

    Fix maps

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,12 +1,11 @@
 $(document).ready(function() {
-	var map = new L.Map('map');
-	var osmUrl = 'http://otile4.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png',
-			osmAttribution = 'Tiles Courtesy of <a href="http://www.mapquest.com/">MapQuest</a> &mdash; Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
-			osm = new L.TileLayer(osmUrl, {maxZoom: 18, attribution: osmAttribution});
+	var map = new L.Map('map', {
+		layers: MQ.mapLayer()
+	});
 
-		var latlng = new L.LatLng(lat, lng);
-	map.setView(latlng, 16).addLayer(osm);
+	var latlng = new L.LatLng(lat, lng);
+	map.setView(latlng, 16);
 
-	var marker = new L.Marker(latlng, {icon: new L.Icon('/assets/marker.png')});
+	var marker = new L.Marker(latlng, {icon: new L.Icon({iconUrl : '/assets/marker.png'})});
 	map.addLayer(marker);
 })
--- app/assets/javascripts/searchmap.js
+++ app/assets/javascripts/searchmap.js
@@ -1,16 +1,15 @@
 $(document).ready(function() {
-	var map = new L.Map('searchmap');
-	var osmUrl = 'http://otile4.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png',
-			osmAttribution = 'Tiles Courtesy of <a href="http://www.mapquest.com/">MapQuest</a> &mdash; Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
-			osm = new L.TileLayer(osmUrl, {maxZoom: 18, attribution: osmAttribution});
+	var map = new L.Map('searchmap', {
+		layers: MQ.mapLayer()
+	});
 
 	var latlng = new L.LatLng(52.6839, -1.82598);
-	map.setView(latlng, 4).addLayer(osm);
+	map.setView(latlng, 4);
 	var mapBounds = new L.LatLngBounds();
 
 	for (i in points) {
 		var latlng = new L.LatLng(points[i]['lat'], points[i]['lng']);
-		var marker = new L.Marker(latlng, {icon: new L.Icon('/assets/'+ i +'.png')});
+		var marker = new L.Marker(latlng, {icon: new L.Icon({ iconUrl: '/assets/'+ i +'.png'})});
 		marker.bindPopup("<h4><a href="+ points[i]['url'] +">"+ points[i]['name'] +"</a></h4><img src='/assets/rating"+points[i]['rating']+".png' /><br />"+ points[i]['rating'] +" stars");
 		map.addLayer(marker);
 		mapBounds.extend(latlng);
